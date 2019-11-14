f8a8b376ea6ec8182b4dfb3382ac6116af8a3710
Jack Hannah <38332502+jackhannah95@users.noreply.github.com>
2019-11-14 16:03:30 +0000

    tiny changes

--- code/01_smr-workshop-code.R
+++ code/01_smr-workshop-code.R
@@ -72,7 +72,7 @@ library(writexl)
 # because `lubridate` was loaded later, `lubridate::here` masks `here::here`
 # To circumvent issues with masking , either edit the order in which packages  
 # are loaded, or explicitly declare which package a function comes from, e.g. 
-# use `lubridate::here` or `here::here` instead of just `here`
+# use `lubridate::here` or `here::here` instead of `here`
 source(here::here("code", "00_sql-queries.R"))
 
 
